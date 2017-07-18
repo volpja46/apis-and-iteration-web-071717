530bbdcd05b638bc3dd3ae0271ab1964f6e8cad0
Tim Freeman <d.timothy.freeman@gmail.com>
2017-07-18 13:56:01 -0400

    refactored API request into single responsibility methods

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,51 +2,94 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-# Returns an array of hashes about each move character has been in
-def get_character_movies_from_api(character)
-  #make the web request
+# web request returning an array of hashes full of character info
+def get_characters_from_api
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
+end
 
-######## iterate over the character hash to find the collection of `films` for the given `character`
-  # cuts data to just results of all characters on page
-  all_character_data = character_hash["results"]
+# cuts data to just results of all characters on page
+def remove_unnecessary_data(info_hash) 
+  just_character_data = info_hash["results"]
+end
   
-  # cuts data to give just info hash of our character to a variable
-  specific_character_data = all_character_data.select do |hash|
+# cuts data to give just info hash of our character to a variable
+def get_specific_character_data(removed_unnecessary_data, character)
+  specific_character_data = removed_unnecessary_data.select do |hash|
     hash["name"].downcase == character
   end
+end
   # returns films for our character in a variable
+def get_movies_of_specific_character(specific_character_data)
   specific_character_films = specific_character_data.first["films"]
+end
 
-
-######## collect those film API urls, make a web request to each URL to get the info for that film --- return value of this method should be collection of info about each film. i.e. an array of hashes in which each hash reps a given film ---this collection will be the argument given to `parse_character_movies` and that method will do some nice presentation stuff: puts out a list of movies by title. play around with puts out other info about a given film.
-  specific_character_films.map do |film|
+######## collect those film API urls, make a web request to each URL to get the info for that film --- return value of this method should be collection of info about each film.
+def get_info_on_films(film_array) 
+  film_array.map do |film|
     film_info = JSON.parse(RestClient.get(film))
     film_info
   end
 end
 
+###############################################################
+#                          BEGIN                              #
+# helper methods to be used in #parse_character_movies method #
+#                                                             #
+###############################################################
+
+#get title of a film
+def film_title(film)
+  film["title"]
+end
+
+#get episode number of a film and turn it into a string instead of an int
+def film_episode(film)
+  film["episode_id"].to_s
+end
+
+#get director of film
+def film_director(film)
+  film["director"]
+end
+
+#get producer(s) of film
+def film_producer(film)
+    film["producer"].split(', ').join(" and ")
+end
+
+#get release date of film
+def film_release_date(film)
+  film["release_date"]
+end
+
+###############################################################
+#                           END                               #
+# helper methods to be used in #parse_character_movies method #
+#                                                             #
+###############################################################
+
+#use helper methods to puts out well-formatted report of movies
 def parse_character_movies(films_hash)
-  # some interation magic and puts out the movies in a nice list
   films_hash.each do |film|
     puts "*" * 20
-    puts film["title"]
-    puts "Episode " + film["episode_id"].to_s
-    puts film["director"]
-    puts film["producer"]
-    puts film["release_date"]
+    puts "Title: #{film_title(film)}"
+    puts "Episode: #{film_episode(film)}"
+    puts "Director: #{film_director(film)}"
+    puts "Producer(s): #{film_producer(film)}"
+    puts "Release Date: #{film_release_date(film)}"
   end
 end
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  all_character_hash = get_characters_from_api
+  removed_extra_data = remove_unnecessary_data(all_character_hash)
+  specific_character_data = get_specific_character_data(removed_extra_data, character)
+  character_movie_data = get_movies_of_specific_character(specific_character_data)
+  film_info = get_info_on_films(character_movie_data)
+  parse_character_movies(film_info)
 end
 
-## TESTING IF METHODS WORK TOGETHER
-# luke_movies = get_character_movies_from_api("Luke Skywalker")
-# parse_character_movies(luke_movies)
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
